06e93a83bfcc4f3e6cbc4fd901b49a52369384ba
Aidan Grabe <aidsgrabe@gmail.com>
2015-01-08 15:38:43 +0000

    Add listeners for save and cancel buttons

--- mobile/src/main/java/com/aidangrabe/studentapp/fragments/NewClassFragment.java
+++ mobile/src/main/java/com/aidangrabe/studentapp/fragments/NewClassFragment.java
@@ -1,13 +1,13 @@
 package com.aidangrabe.studentapp.fragments;
 
 import android.app.Fragment;
-import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TimePicker;
+import android.widget.Toast;
 
 import com.aidangrabe.studentapp.R;
 import com.aidangrabe.studentapp.ui.TimeEditText;
@@ -19,11 +19,16 @@ import com.aidangrabe.studentapp.ui.TimeEditText;
 public class NewClassFragment extends Fragment {
 
     private TimeEditText mFromEditText, mToEditText;
+    private Button mSaveButton, mCancelButton;
 
-    private final TimePickerDialog.OnTimeSetListener mFromTimeSetListener = new TimePickerDialog.OnTimeSetListener() {
+    private final View.OnClickListener mSaveCancelClickListener = new View.OnClickListener() {
         @Override
-        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-
+        public void onClick(View v) {
+            if (v.equals(mSaveButton)) {
+                onSavePressed();
+            } else {
+                onCancelPressed();
+            }
         }
     };
 
@@ -37,11 +42,27 @@ public class NewClassFragment extends Fragment {
         mFromEditText = new TimeEditText(getActivity(), fromText, savedInstanceState);
         mToEditText = new TimeEditText(getActivity(), toText, savedInstanceState);
 
+        mCancelButton = (Button) view.findViewById(R.id.cancel_button);
+        mSaveButton = (Button) view.findViewById(R.id.save_button);
+
+        mSaveButton.setOnClickListener(mSaveCancelClickListener);
+        mCancelButton.setOnClickListener(mSaveCancelClickListener);
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_new_class, container, false);
+    }
+
+    // called when the save button is pressed
+    public void onSavePressed() {
+        Toast.makeText(getActivity(), "Saving!", Toast.LENGTH_SHORT).show();
+    }
+
+    // called when the cancel button is pressed
+    public void onCancelPressed() {
+        getActivity().finish();
     }
 
     @Override
