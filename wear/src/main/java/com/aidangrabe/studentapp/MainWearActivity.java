0783f6460ed29fcf6cc170c9fb8f718ee095fc2f
Aidan Grabe <aidsgrabe@gmail.com>
2015-01-09 00:25:48 +0000

    Add main menu to Wear app

--- wear/src/main/java/com/aidangrabe/studentapp/MainWearActivity.java
+++ wear/src/main/java/com/aidangrabe/studentapp/MainWearActivity.java
@@ -1,24 +1,95 @@
 package com.aidangrabe.studentapp;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.wearable.view.WatchViewStub;
+import android.support.wearable.view.WearableListView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
-public class MainWearActivity extends Activity {
+public class MainWearActivity extends Activity implements WearableListView.ClickListener {
 
-    private TextView mTextView;
+    private WearableListView mListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_wear);
-        final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
-        stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
-            @Override
-            public void onLayoutInflated(WatchViewStub stub) {
-                mTextView = (TextView) stub.findViewById(R.id.text);
-            }
-        });
+
+        setContentView(R.layout.activity_main_menu);
+
+        String[] menuOptions = getResources().getStringArray(R.array.main_menu_options);
+
+        mListView = (WearableListView) findViewById(R.id.wearable_list);
+        mListView.setAdapter(new Adapter(this, menuOptions));
+        mListView.setClickListener(this);
+
     }
+
+    @Override
+    public void onClick(WearableListView.ViewHolder viewHolder) {
+        Log.d("DEBUG", "onClick!");
+    }
+
+    @Override
+    public void onTopEmptyRegionClick() {
+        Log.d("DEBUG", "onTopEmptyRegionClick!");
+    }
+
+    private static class Adapter extends WearableListView.Adapter {
+
+        private Context mContext;
+        private LayoutInflater mInflater;
+        private String[] mDataset;
+
+        public Adapter(Context context, String[] dataset) {
+            mContext = context;
+            mInflater = LayoutInflater.from(context);
+            mDataset = dataset;
+        }
+
+        // Provide a reference to the type of views you're using
+        public static class ItemViewHolder extends WearableListView.ViewHolder {
+            private TextView textView;
+            public ItemViewHolder(View itemView) {
+                super(itemView);
+                // find the text view within the custom item's layout
+                textView = (TextView) itemView.findViewById(R.id.name);
+            }
+        }
+        // Create new views for list items
+        // (invoked by the WearableListView's layout manager)
+        @Override
+        public WearableListView.ViewHolder onCreateViewHolder(ViewGroup parent,
+                                                              int viewType) {
+            // Inflate our custom layout for list items
+            return new ItemViewHolder(mInflater.inflate(R.layout.list_item_main_menu, null));
+        }
+
+        // Replace the contents of a list item
+        // Instead of creating new views, the list tries to recycle existing ones
+        // (invoked by the WearableListView's layout manager)
+        @Override
+        public void onBindViewHolder(WearableListView.ViewHolder holder,
+                                     int position) {
+            // retrieve the text view
+            ItemViewHolder itemHolder = (ItemViewHolder) holder;
+            TextView view = itemHolder.textView;
+            // replace text contents
+            view.setText(mDataset[position]);
+            // replace list item's metadata
+            holder.itemView.setTag(position);
+        }
+
+        // Return the size of your dataset
+        // (invoked by the WearableListView's layout manager)
+        @Override
+        public int getItemCount() {
+            return mDataset.length;
+        }
+    }
+
 }
