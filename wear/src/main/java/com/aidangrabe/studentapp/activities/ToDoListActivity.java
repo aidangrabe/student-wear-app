cef81f58bf557b3d9f4c05b5d7e2f1415d4021d6
Aidan Grabe <aidsgrabe@gmail.com>
2015-01-10 17:03:56 +0000

    Update ToDoListActivity for wear to show ToDoItems in it's list

--- wear/src/main/java/com/aidangrabe/studentapp/activities/ToDoListActivity.java
+++ wear/src/main/java/com/aidangrabe/studentapp/activities/ToDoListActivity.java
@@ -1,9 +1,15 @@
 package com.aidangrabe.studentapp.activities;
 
 import android.app.Activity;
+import android.graphics.Paint;
 import android.os.Bundle;
-import android.util.Log;
+import android.os.Handler;
+import android.os.Looper;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.aidangrabe.common.SharedConstants;
 import com.aidangrabe.common.model.todolist.ToDoItem;
@@ -11,18 +17,15 @@ import com.aidangrabe.common.model.todolist.ToDoItemManager;
 import com.aidangrabe.common.wearable.WearUtil;
 import com.aidangrabe.studentapp.R;
 import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.PendingResult;
-import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.wearable.DataApi;
 import com.google.android.gms.wearable.DataEvent;
 import com.google.android.gms.wearable.DataEventBuffer;
 import com.google.android.gms.wearable.DataItem;
-import com.google.android.gms.wearable.DataItemBuffer;
 import com.google.android.gms.wearable.DataMap;
-import com.google.android.gms.wearable.DataMapItem;
 import com.google.android.gms.wearable.Wearable;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 /**
@@ -36,6 +39,13 @@ public class ToDoListActivity extends Activity implements GoogleApiClient.Connec
     private ListView mListView;
     private WearUtil mWearUtil;
     private List<ToDoItem> mItems;
+    private ArrayAdapter<ToDoItem> mAdapter;
+    private final Comparator<ToDoItem> mAdapterComparator = new Comparator<ToDoItem>() {
+        @Override
+        public int compare(ToDoItem lhs, ToDoItem rhs) {
+            return rhs.getCreationDate().compareTo(lhs.getCreationDate());
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,12 +55,34 @@ public class ToDoListActivity extends Activity implements GoogleApiClient.Connec
 
         mListView = (ListView) findViewById(R.id.list_view);
 
-        Log.d("DEBUG", "Create WearUtils");
         mWearUtil = new WearUtil(this)
                 .setConnectionCallbacks(this)
                 .setDataListener(this);
 
         mItems = new ArrayList<>();
+
+        mAdapter = new ArrayAdapter<ToDoItem>(this, android.R.layout.simple_list_item_1) {
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                View view = super.getView(position, convertView, parent);
+
+                ToDoItem item = mAdapter.getItem(position);
+
+                TextView tv = (TextView) view.findViewById(android.R.id.text1);
+                tv.setTextColor(getResources().getColor(R.color.default_text_color));
+                tv.setText(item.getTitle());
+
+                // toggle strike through
+                if (item.isCompleted()) {
+                    tv.setPaintFlags(tv.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
+                } else {
+                    tv.setPaintFlags(tv.getPaintFlags() & ~Paint.STRIKE_THRU_TEXT_FLAG);
+                }
+
+                return view;
+            }
+        };
+        mListView.setAdapter(mAdapter);
 
     }
 
@@ -72,11 +104,9 @@ public class ToDoListActivity extends Activity implements GoogleApiClient.Connec
 
     public void getToDoItems() {
 
-        Log.d("DEBUG", "Send Message");
         mWearUtil.sendMessage(MESSAGE_REQUEST_TODO_ITEMS, "");
 
-        mItems.clear();
-        PendingResult<DataItemBuffer> results = Wearable.DataApi.getDataItems(mWearUtil.getGoogleApiClient());
+        Wearable.DataApi.getDataItems(mWearUtil.getGoogleApiClient());
 
     }
 
@@ -95,14 +125,21 @@ public class ToDoListActivity extends Activity implements GoogleApiClient.Connec
     @Override
     public void onDataChanged(DataEventBuffer dataEvents) {
 
-        Log.d("DEBUG", "onDataChanged");
-        mItems.clear();
         for (DataEvent event : dataEvents) {
             DataItem dataItem = event.getDataItem();
             if (dataItem.getUri().getPath().equals(SharedConstants.Wearable.MESSAGE_REQUEST_TODO_ITEMS)) {
-                ToDoItem item = ToDoItemManager.fromDataMap(DataMap.fromByteArray(event.getDataItem().getData()));
-                mItems.add(item);
-                Log.d("DEBUG", item.getTitle());
+                final ToDoItem item = ToDoItemManager.fromDataMap(DataMap.fromByteArray(event.getDataItem().getData()));
+                if (!mItems.contains(item)) {
+                    mItems.add(item);
+                    new Handler(Looper.getMainLooper()).post(new Runnable() {
+                        @Override
+                        public void run() {
+                            mAdapter.notifyDataSetChanged();
+                            mAdapter.add(item);
+                            mAdapter.sort(mAdapterComparator);
+                        }
+                    });
+                }
             }
         }
 
