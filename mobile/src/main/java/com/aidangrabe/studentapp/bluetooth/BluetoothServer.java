57faa92e65f82fbbfb80051b79edd373a8b812d5
Aidan Grabe <aidsgrabe@gmail.com>
2015-01-20 19:04:06 +0000

    Add BluetoothServer for Bluetooth communication

---
+++ mobile/src/main/java/com/aidangrabe/studentapp/bluetooth/BluetoothServer.java
@@ -0,0 +1,204 @@
+package com.aidangrabe.studentapp.bluetooth;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothServerSocket;
+import android.bluetooth.BluetoothSocket;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
+
+/**
+ * Created by aidan on 20/01/15.
+ *
+ */
+public class BluetoothServer {
+
+    public static final String SERVICE_NAME = "BluetoothService";
+    public static final String SERVICE_UUID = "4b6e3550-9f63-11e4-bcd8-0800200c9a66";
+
+    private static volatile int clientId = 0;
+
+    private BluetoothAdapter mBtAdapter;
+    private BluetoothServerSocket mServerSocket;
+    private List<BluetoothDevice> mDevices;
+    private ServerHandler mServerHandler;
+    private List<BluetoothListener> mListeners;
+
+    public interface BluetoothListener {
+        public void onMessageReceived(ClientHandler client, String message);
+        public void onClientConnected(int clientId);
+        public void onClientDisconnected(int clientId);
+    }
+
+    public BluetoothServer() {
+
+        mDevices = new ArrayList<>();
+        mListeners = new ArrayList<>();
+
+        mBtAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        // check if bluetooth is available
+        if (mBtAdapter == null) {
+            throw new IllegalStateException("No bluetooth available");
+        }
+
+        try {
+            mServerSocket = mBtAdapter.listenUsingInsecureRfcommWithServiceRecord(SERVICE_NAME, UUID.fromString(SERVICE_UUID));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void start() {
+
+        mServerHandler = new ServerHandler();
+        new Thread(mServerHandler).start();
+
+    }
+
+    public void stop() {
+
+        mServerHandler.stop();
+        try {
+            mServerSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void bluetoothListPairedDevices() {
+
+        Set<BluetoothDevice> devices = mBtAdapter.getBondedDevices();
+        for (BluetoothDevice device : devices) {
+            mDevices.add(device);
+        }
+
+    }
+
+    public void addBluetoothListener(BluetoothListener listener) {
+        mListeners.add(listener);
+    }
+
+    public void removeBluetoothListener(BluetoothListener listener) {
+        mListeners.remove(listener);
+    }
+
+    private class ServerHandler implements Runnable {
+        private boolean running;
+
+        public ServerHandler() {
+            Log.d("D", "New Server Handler");
+            running = true;
+        }
+
+        @Override
+        public void run() {
+            Log.d("D", "Server running");
+            while(running) {
+                Log.d("D", "run loop");
+                try {
+                    BluetoothSocket client = mServerSocket.accept();
+                    new Thread(new ClientHandler(client)).start();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        public void stop() {
+            running = false;
+        }
+
+    }
+
+    private class ClientHandler implements Runnable {
+
+        private int id;
+        private BluetoothSocket mSocket;
+        private PrintWriter mOutWriter;
+        private BufferedReader mInReader;
+        private boolean running;
+
+        public ClientHandler(BluetoothSocket socket) {
+            Log.d("D", "New ClientHandler");
+            mSocket = socket;
+            running = true;
+            id = clientId++;
+
+            for (BluetoothListener listener : mListeners) {
+                listener.onClientConnected(id);
+            }
+
+            try {
+                mInReader = new BufferedReader(new InputStreamReader(mSocket.getInputStream()));
+                mOutWriter = new PrintWriter(mSocket.getOutputStream(), true);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void run() {
+
+            while (running) {
+                String line = null;
+                try {
+                    line = mInReader.readLine();
+                    if (line == null) {
+                        running = false;
+                        break;
+                    }
+
+                    for (BluetoothListener listener : mListeners) {
+                        listener.onMessageReceived(this, line);
+                    }
+                } catch (IOException e) {
+                    for (BluetoothListener listener : mListeners) {
+                        listener.onClientDisconnected(id);
+                    }
+                    Log.d("D", "Client disconnected");
+                    running = false;
+                    break;
+                }
+                Log.d("D", "Server received line [id=" + id + "]: " + line);
+            }
+            Log.d("D", "Closed Client");
+            disconnect();
+
+        }
+
+        public void disconnect() {
+
+            try {
+                mSocket.close();
+                mInReader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            mOutWriter.close();
+
+        }
+
+        public void setRunning(boolean running) {
+            this.running = running;
+        }
+
+        public void write(String msg) {
+            mOutWriter.println(msg);
+            mOutWriter.flush();
+        }
+
+    }
+
+}
+
