e2a30c9da43d60000c33554c9ab687d7c89361e4
Aidan Grabe <aidsgrabe@gmail.com>
2015-01-31 16:10:56 +0000

    Change mobile app main menu layout
    - Main menu now has icons and descriptions for each item
    - "+ Add Class" menu option is now gone, to be replaced with "Modules"
    - Also added ic_timetable.png for the "Timetable" menu option icon

--- mobile/src/main/java/com/aidangrabe/studentapp/fragments/MainMenuFragment.java
+++ mobile/src/main/java/com/aidangrabe/studentapp/fragments/MainMenuFragment.java
@@ -1,23 +1,23 @@
 package com.aidangrabe.studentapp.fragments;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
-import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import com.aidangrabe.studentapp.R;
-import com.aidangrabe.studentapp.activities.NewClassActivity;
 import com.aidangrabe.studentapp.activities.TimeTableActivity;
 import com.aidangrabe.studentapp.activities.ToDoListActivity;
 import com.aidangrabe.studentapp.activities.games.SnakeActivity;
-
-import java.util.LinkedHashMap;
-import java.util.Map;
 
 
 /**
@@ -27,20 +27,28 @@ import java.util.Map;
 public class MainMenuFragment extends ListFragment {
 
     // the list's adapter
-    private ArrayAdapter<String> mAdapter;
-    private Map<String, Class> mMenuMap;
+    private ArrayAdapter<MenuItem> mAdapter;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        mAdapter = new ArrayAdapter<String>(getActivity().getApplicationContext(), android.R.layout.simple_list_item_1) {
+        mAdapter = new ArrayAdapter<MenuItem>(getActivity().getApplicationContext(), android.R.layout.simple_list_item_1) {
             @Override
             public View getView(int position, View convertView, ViewGroup parent) {
-                View view = super.getView(position, convertView, parent);
+                View view = convertView;
+                if (view == null) {
+                    view = LayoutInflater.from(getActivity()).inflate(R.layout.list_item_main_menu, parent, false);
+                }
 
-                TextView tv = (TextView) view.findViewById(android.R.id.text1);
-                tv.setTextColor(getResources().getColor(R.color.DefaultTextColor));
+                MenuItem menuItem = getItem(position);
+                TextView tv = (TextView) view.findViewById(R.id.title_text);
+                TextView tvDescription = (TextView) view.findViewById(R.id.description_text);
+                ImageView iconView = (ImageView) view.findViewById(R.id.icon);
+
+                tv.setText(menuItem.getTitle());
+                tvDescription.setText(menuItem.getDescription());
+                iconView.setImageBitmap(menuItem.getIcon());
 
                 return view;
             }
@@ -48,7 +56,7 @@ public class MainMenuFragment extends ListFragment {
 
         createMenu();
         setListAdapter(mAdapter);
-        refreshAdapter();
+        mAdapter.notifyDataSetChanged();
 
     }
 
@@ -61,36 +69,75 @@ public class MainMenuFragment extends ListFragment {
     public void onListItemClick(ListView l, View v, int position, long id) {
         super.onListItemClick(l, v, position, id);
 
-        String item = mAdapter.getItem(position);
-
-        if (mMenuMap.containsKey(item)) {
-            startActivity(mMenuMap.get(item));
-        }
-
-    }
-
-    public void startActivity(Class c) {
-        Intent intent = new Intent(getActivity(), c);
+        MenuItem item = mAdapter.getItem(position);
+        Intent intent = new Intent(getActivity(), item.getActivityClass());
         startActivity(intent);
+
     }
 
     private void createMenu() {
 
-        mMenuMap = new LinkedHashMap<>();
-        mMenuMap.put(getResources().getString(R.string.menu_add_class), NewClassActivity.class);
-        mMenuMap.put(getResources().getString(R.string.menu_todo_list), ToDoListActivity.class);
-        mMenuMap.put(getResources().getString(R.string.menu_timetable), TimeTableActivity.class);
-        mMenuMap.put(getResources().getString(R.string.menu_games), SnakeActivity.class);
+        //TODO: Modules
+//        mAdapter.add(new MenuItem(R.string.menu_todo_list, R.string.menu_todo_list, R.drawable.ic_todo, ToDoListActivity.class));
+        mAdapter.add(new MenuItem(R.string.menu_todo_list, R.string.menu_todo_list_description, R.drawable.ic_todo, ToDoListActivity.class));
+        mAdapter.add(new MenuItem(R.string.menu_timetable, R.string.menu_timetable_description, R.drawable.ic_timetable, TimeTableActivity.class));
+        mAdapter.add(new MenuItem(R.string.menu_games, R.string.menu_games_description, R.drawable.ic_games, SnakeActivity.class));
 
     }
 
-    // reload the data in the adapter
-    public void refreshAdapter() {
+    /**
+     * This class represents a single menu item in a list of menu options
+     * Each menu item contains a title, description, icon and Activity class to start when selected
+     */
+    protected class MenuItem {
 
-        mAdapter.clear();
-        mAdapter.addAll(mMenuMap.keySet());
-        mAdapter.notifyDataSetChanged();
+        private String mTitle, mDescription;
+        private Bitmap mIcon;
+        private Class<? extends Activity> mActivityClass;
 
+        public MenuItem(int titleRes, int descRes, int iconRes, Class<? extends Activity> activityClass) {
+            this(getResources().getString(titleRes), getResources().getString(descRes),
+                    BitmapFactory.decodeResource(getResources(), iconRes), activityClass);
+        }
+
+        public MenuItem(String title, String description, Bitmap iconBitmap, Class<? extends Activity> activityClass) {
+            mTitle = title;
+            mDescription = description;
+            mIcon = iconBitmap;
+            mActivityClass = activityClass;
+        }
+
+        public String getTitle() {
+            return mTitle;
+        }
+
+        public void setTitle(String mTitle) {
+            this.mTitle = mTitle;
+        }
+
+        public String getDescription() {
+            return mDescription;
+        }
+
+        public void setDescription(String mDescription) {
+            this.mDescription = mDescription;
+        }
+
+        public Bitmap getIcon() {
+            return mIcon;
+        }
+
+        public void setIcon(Bitmap mIcon) {
+            this.mIcon = mIcon;
+        }
+
+        public Class<? extends Activity> getActivityClass() {
+            return mActivityClass;
+        }
+
+        public void setActivityClass(Class<? extends Activity> mActivityClass) {
+            this.mActivityClass = mActivityClass;
+        }
     }
 
 }
