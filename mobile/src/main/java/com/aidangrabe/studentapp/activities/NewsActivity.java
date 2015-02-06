634efee25722dc6a96c450e9ddfc9811e63bf5d2
Aidan Grabe <aidsgrabe@gmail.com>
2015-02-06 01:03:15 +0000

    Refactor NewsActivity to use Fragment that displays RecyclerView instead of ListView
    - Add CardView and RecyclerView as dependancies for gradle

--- mobile/src/main/java/com/aidangrabe/studentapp/activities/NewsActivity.java
+++ mobile/src/main/java/com/aidangrabe/studentapp/activities/NewsActivity.java
@@ -1,142 +1,27 @@
 package com.aidangrabe.studentapp.activities;
 
-import android.content.Intent;
-import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.animation.AlphaAnimation;
-import android.view.animation.Animation;
-import android.view.animation.DecelerateInterpolator;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ImageView;
-import android.widget.ListView;
-import android.widget.TextView;
+import android.support.v4.app.Fragment;
 
-import com.aidangrabe.common.model.Article;
-import com.aidangrabe.common.news.ArticleFetcher;
-import com.aidangrabe.common.news.UccArticleFetcher;
-import com.aidangrabe.common.util.MyVolley;
-import com.aidangrabe.studentapp.R;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.ImageRequest;
-
-import java.lang.ref.WeakReference;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.List;
+import com.aidangrabe.common.activities.FragContainerActivity;
+import com.aidangrabe.studentapp.fragments.NewsFragment;
 
 /**
  * Created by aidan on 04/02/15.
  * This Activity displays a list of news articles fetched by an ArticleFetcher
  */
-public class NewsActivity extends ActionBarActivity implements AdapterView.OnItemClickListener {
-
-    private ListView mListView;
-    private ArrayAdapter<Article> mAdapter;
-    private ArticleFetcher mArticleFetcher;
-    private SimpleDateFormat mDateFormat;
+public class NewsActivity extends FragContainerActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.list_view);
-
         setTitle("News");
-
-        mDateFormat = new SimpleDateFormat("MMM dd yy");
-
-        mArticleFetcher = new UccArticleFetcher(this);
-        mArticleFetcher.fetchArticles(new ArticleFetcher.Listener() {
-            @Override
-            public void onArticlesReady(List<Article> articles) {
-                refreshList(articles);
-            }
-        });
-
-
-        setupListView();
-
-    }
-
-    private void refreshList(List<Article> articles) {
-
-        mAdapter.clear();
-        mAdapter.addAll(articles);
-        mAdapter.notifyDataSetChanged();
-
-    }
-
-    /**
-     * Get the ListView and set it up with it's Adapter
-     */
-    private void setupListView() {
-        mListView = (ListView) findViewById(R.id.list_view);
-        mAdapter = new ArrayAdapter<Article>(this, android.R.layout.simple_list_item_1) {
-            @Override
-            public View getView(int position, View convertView, ViewGroup parent) {
-                if (convertView == null) {
-                    convertView = LayoutInflater.from(NewsActivity.this).inflate(R.layout.list_item_article, parent, false);
-                }
-                Article article = getItem(position);
-                Date date = new Date(article.getPublishTime());
-                ((TextView) convertView.findViewById(R.id.title_text)).setText(article.getTitle());
-                ((TextView) convertView.findViewById(R.id.date_text)).setText(mDateFormat.format(date));
-                ImageView imageView = (ImageView) convertView.findViewById(R.id.image_view);
-                if (article.getImage() == null) {
-                    imageView.setVisibility(View.INVISIBLE);
-                    downloadImage(article, imageView);
-                } else {
-                    imageView.setVisibility(View.VISIBLE);
-                    imageView.setImageBitmap(article.getImage());
-                }
-                return convertView;
-            }
-        };
-        mListView.setAdapter(mAdapter);
-        mListView.setOnItemClickListener(this);
-    }
-
-    private void downloadImage(final Article article, ImageView imageView) {
-
-        final WeakReference<ImageView> imageViewWeakReference = new WeakReference<ImageView>(imageView);
-
-        MyVolley.getInstance(this).add(new ImageRequest(article.getImageUrl(), new Response.Listener<Bitmap>() {
-            @Override
-            public void onResponse(Bitmap response) {
-                ImageView imageV = imageViewWeakReference.get();
-                if (imageV != null) {
-                    imageV.setImageBitmap(response);
-                    Animation alphaAnimation = new AlphaAnimation(0, 1);
-                    alphaAnimation.setInterpolator(new DecelerateInterpolator(3f));
-                    alphaAnimation.setDuration(1000);
-                    imageV.startAnimation(alphaAnimation);
-                    imageV.setVisibility(View.VISIBLE);
-                    article.setImage(response);
-                }
-            }
-        }, 320, 320, Bitmap.Config.ARGB_4444, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-
-            }
-        }));
 
     }
 
     @Override
-    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
-        Article article = mAdapter.getItem(position);
-
-        Intent intent = new Intent(this, NewsArticleActivity.class);
-        intent.putExtra(NewsArticleActivity.EXTRA_ARTICLE_URL, article.getLink());
-        startActivity(intent);
-
+    protected Fragment onCreateFragment() {
+        return new NewsFragment();
     }
 }
---
+++ mobile/src/main/java/com/aidangrabe/studentapp/fragments/NewsFragment.java
@@ -0,0 +1,181 @@
+package com.aidangrabe.studentapp.fragments;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.DecelerateInterpolator;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.aidangrabe.common.model.Article;
+import com.aidangrabe.common.news.ArticleFetcher;
+import com.aidangrabe.common.news.UccArticleFetcher;
+import com.aidangrabe.common.util.MyVolley;
+import com.aidangrabe.studentapp.R;
+import com.aidangrabe.studentapp.activities.NewsArticleActivity;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.ImageRequest;
+
+import java.lang.ref.WeakReference;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by aidan on 05/02/15.
+ * This Fragment displays a list of Articles that start the NewsArticleActivity
+ * when clicked.
+ */
+public class NewsFragment extends Fragment implements View.OnClickListener {
+
+    private RecyclerView.LayoutManager mLayoutManager;
+    private RecyclerView mRecyclerView;
+    private SimpleDateFormat mDateFormat;
+    private Date mDate;
+    private List<Article> mArticles;
+    private RecyclerView.Adapter<Adapter.ViewHolder> mAdapter;
+    private ArticleFetcher mArticleFetcher;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mDateFormat = new SimpleDateFormat("MMM dd yy");
+
+        mArticles = new ArrayList<>();
+        mDate = new Date();
+
+        mArticleFetcher = new UccArticleFetcher(getActivity());
+        mArticleFetcher.fetchArticles(new ArticleFetcher.Listener() {
+            @Override
+            public void onArticlesReady(List<Article> articles) {
+                mArticles = articles;
+                mAdapter.notifyDataSetChanged();
+            }
+        });
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_news, container, false);
+
+        mRecyclerView = (RecyclerView) view.findViewById(R.id.recycler_view);
+        mLayoutManager = new LinearLayoutManager(getActivity());
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        mAdapter = new Adapter();
+        mRecyclerView.setAdapter(mAdapter);
+
+        return view;
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        int position = mRecyclerView.getChildPosition(v);
+        Article article = mArticles.get(position);
+
+        Intent intent = new Intent(getActivity(), NewsArticleActivity.class);
+        intent.putExtra(NewsArticleActivity.EXTRA_ARTICLE_URL, article.getLink());
+        startActivity(intent);
+
+    }
+
+    public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+            View view = LayoutInflater.from(getActivity()).inflate(R.layout.recycler_item_news, parent, false);
+            view.setOnClickListener(NewsFragment.this);
+
+            return new ViewHolder(view);
+
+        }
+
+        @Override
+        public void onBindViewHolder(ViewHolder holder, int position) {
+
+            Article article = mArticles.get(position);
+
+            mDate.setTime(article.getPublishTime());
+
+            holder.titleTextView.setText(article.getTitle());
+            holder.summaryTextView.setText(mDateFormat.format(mDate));
+
+            if (article.getImage() == null) {
+                downloadImage(article, holder.imageView);
+                holder.imageView.setVisibility(View.INVISIBLE);
+            } else {
+                holder.imageView.setVisibility(View.VISIBLE);
+                holder.imageView.setImageBitmap(article.getImage());
+            }
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return mArticles.size();
+        }
+
+        public class ViewHolder extends RecyclerView.ViewHolder {
+
+            public TextView titleTextView, summaryTextView;
+            public ImageView imageView;
+
+            public ViewHolder(View itemView) {
+                super(itemView);
+
+                titleTextView = (TextView) itemView.findViewById(R.id.title_text);
+                summaryTextView = (TextView) itemView.findViewById(R.id.summary_text);
+                imageView = (ImageView) itemView.findViewById(R.id.image);
+
+            }
+
+        }
+
+    }
+
+    private void downloadImage(final Article article, ImageView imageView) {
+
+        final WeakReference<ImageView> imageViewWeakReference = new WeakReference<>(imageView);
+
+        MyVolley.getInstance(getActivity()).add(new ImageRequest(article.getImageUrl(), new Response.Listener<Bitmap>() {
+            @Override
+            public void onResponse(Bitmap response) {
+                ImageView imageV = imageViewWeakReference.get();
+                if (imageV != null) {
+                    imageV.setImageBitmap(response);
+                    Animation alphaAnimation = new AlphaAnimation(0, 1);
+                    alphaAnimation.setInterpolator(new DecelerateInterpolator(3f));
+                    alphaAnimation.setDuration(1000);
+                    imageV.startAnimation(alphaAnimation);
+                    imageV.setVisibility(View.VISIBLE);
+                    article.setImage(response);
+                }
+            }
+        }, 320, 320, Bitmap.Config.ARGB_4444, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        }));
+
+    }
+
+
+}
