7b81c708ee735ce17f34080f33b1929d42c9c56c
Aidan Grabe <aidsgrabe@gmail.com>
2015-02-07 12:58:57 +0000

    Result implements Parcelable

--- common/src/main/java/com/aidangrabe/common/model/Result.java
+++ common/src/main/java/com/aidangrabe/common/model/Result.java
@@ -1,5 +1,8 @@
 package com.aidangrabe.common.model;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import com.orm.SugarRecord;
 
 import java.util.Date;
@@ -8,7 +11,7 @@ import java.util.Date;
  * Created by aidan on 02/02/15.
  * This class describes a particular result for a given module
  */
-public class Result extends SugarRecord<Result> {
+public class Result extends SugarRecord<Result> implements Parcelable {
 
     // the module this result belongs to
     private Module module;
@@ -50,4 +53,31 @@ public class Result extends SugarRecord<Result> {
     public void setCreateTime(long createTime) {
         this.createTime = createTime;
     }
+
+    // Parcelable methods
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeFloat(grade);
+        dest.writeLong(getId());
+    }
+
+    public static final Creator<Result> CREATOR = new Creator<Result>() {
+        @Override
+        public Result createFromParcel(Parcel source) {
+            Result result = new Result(source.readFloat());
+            result.setId(source.readLong());
+            return result;
+        }
+
+        @Override
+        public Result[] newArray(int size) {
+            return new Result[0];
+        }
+    };
+
 }
