d5eb39ff683d44d8c21faf6cb044160fc2ab47d3
Aidan Grabe <aidsgrabe@gmail.com>
2015-02-08 00:09:35 +0000

    Add TimeTableService which gets called every 10 minutes to check if there is a     Lecture starting soon and posts a notification if necessary

--- mobile/src/main/java/com/aidangrabe/studentapp/MainActivity.java
+++ mobile/src/main/java/com/aidangrabe/studentapp/MainActivity.java
@@ -3,23 +3,25 @@ package com.aidangrabe.studentapp;
 import android.app.AlarmManager;
 import android.app.Application;
 import android.app.PendingIntent;
+import android.app.Service;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.SystemClock;
 import android.support.v7.app.ActionBarActivity;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.aidangrabe.studentapp.fragments.MainMenuFragment;
 import com.aidangrabe.studentapp.services.NewsDownloaderService;
+import com.aidangrabe.studentapp.services.TimeTableService;
 import com.aidangrabe.studentapp.wearable.DataLayerListenerService;
 
 
 public class MainActivity extends ActionBarActivity {
 
-    private static final String PREF_NEWS_ALARM_ACTIVE = "news_alarm_active";
+    private static final String PREF_NEWS_ALARM_ACTIVE = "alarm.news";
+    private static final String PREF_TIMETABLE_ALARM_ACTIVE = "alarm.timetable";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,29 +40,35 @@ public class MainActivity extends ActionBarActivity {
                     .commit();
         }
 
-        setupAlarm(getApplication());
+        setupAlarms(getApplication());
 
     }
 
-    private void setupAlarm(Application context) {
+    private void setupAlarms(Application context) {
 
         SharedPreferences prefs = getPreferences(MODE_PRIVATE);
-        boolean alarmUp = prefs.getBoolean(PREF_NEWS_ALARM_ACTIVE, false);
+        boolean newsAlarmOn = prefs.getBoolean(PREF_NEWS_ALARM_ACTIVE, false);
+        boolean timetableAlarmOn = prefs.getBoolean(PREF_TIMETABLE_ALARM_ACTIVE, false);
 
-        if (alarmUp) {
-            Log.d("D", "Alarm is already active");
-        } else {
-            Log.d("D", "Alarm not already active");
-            Intent intent = new Intent(context, NewsDownloaderService.class);
-            PendingIntent pendingIntent = PendingIntent.getService(context, 0, intent, 0);
+        AlarmManager alarmManager = (AlarmManager) context.getSystemService(ALARM_SERVICE);
 
-            AlarmManager alarmManager = (AlarmManager) context.getSystemService(ALARM_SERVICE);
-            alarmManager.setRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP, SystemClock.elapsedRealtime(), 1000 * 60 * 60, pendingIntent);
-
-            // update the prefs
+        if (!newsAlarmOn) {
+            setAlarm(alarmManager, NewsDownloaderService.class, 1000 * 60 * 60);
             prefs.edit().putBoolean(PREF_NEWS_ALARM_ACTIVE, true).apply();
         }
 
+        if (!timetableAlarmOn) {
+            setAlarm(alarmManager, TimeTableService.class, 1000 * 60 * 10);
+            prefs.edit().putBoolean(PREF_TIMETABLE_ALARM_ACTIVE, true).apply();
+        }
+
+    }
+
+    private void setAlarm(AlarmManager alarmManager, Class<? extends Service> serviceClass, long interval) {
+        Intent intent = new Intent(this, serviceClass);
+        PendingIntent pendingIntent = PendingIntent.getService(this, 0, intent, 0);
+
+        alarmManager.setInexactRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP, SystemClock.elapsedRealtime(), interval, pendingIntent);
     }
 
     @Override
---
+++ mobile/src/main/java/com/aidangrabe/studentapp/services/TimeTableService.java
@@ -0,0 +1,96 @@
+package com.aidangrabe.studentapp.services;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.v4.app.NotificationCompat;
+
+import com.aidangrabe.studentapp.R;
+import com.aidangrabe.studentapp.activities.TimeTableActivity;
+import com.aidangrabe.studentapp.models.Lecture;
+
+import java.util.Calendar;
+import java.util.List;
+
+/**
+ * Created by aidan on 07/02/15.
+ * This class creates notifications if a given Lecture is starting soon
+ */
+public class TimeTableService extends Service {
+
+    // the time in minutes before a lecture to show the notification
+    private static int DELTA_TIME = 10;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        List<Lecture> lectures = Lecture.listAll(Lecture.class);
+
+        if (lectures.size() == 0) {
+            finish();
+        }
+
+        Calendar calendar = Calendar.getInstance();
+        int hour = calendar.get(Calendar.HOUR_OF_DAY);
+        int minutes = calendar.get(Calendar.MINUTE);
+
+        Lecture nextLecture = null;
+
+        for (Lecture lecture : lectures) {
+            long delta = toMinutes(lecture) - toMinutes(hour, minutes);
+            if (delta > 0 && delta < DELTA_TIME) {
+                nextLecture = lecture;
+            }
+        }
+
+        if (nextLecture != null) {
+            showNotification(nextLecture);
+        }
+
+        finish();
+
+    }
+
+    private long toMinutes(Lecture lecture) {
+        return toMinutes(lecture.getStartHour(), lecture.getStartMinute());
+    }
+
+    private long toMinutes(int hours, int minutes) {
+        return hours * 60 + minutes;
+    }
+
+    private void showNotification(Lecture lecture) {
+
+        Intent intent = new Intent(this, TimeTableActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+
+        Notification notif = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.drawable.ic_launcher)
+                .setContentTitle(String.format("%d:%01d - %s", lecture.getStartHour(), lecture.getStartMinute(),
+                        lecture.getName()))
+                .setContentText(lecture.getLocation())
+                .setContentIntent(pendingIntent)
+                .build();
+
+        notif.flags = notif.flags | Notification.FLAG_AUTO_CANCEL;
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        notificationManager.notify(001, notif);
+
+    }
+
+    private void finish() {
+        stopSelf();
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+
+}
