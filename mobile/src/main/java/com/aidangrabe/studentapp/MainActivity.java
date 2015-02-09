18d9fdbf9c900460992fadae20af807e8f87b407
Aidan Grabe <aidsgrabe@gmail.com>
2015-02-09 18:48:03 +0000

    Reset alarms every time there interval has elapsed

--- mobile/src/main/java/com/aidangrabe/studentapp/MainActivity.java
+++ mobile/src/main/java/com/aidangrabe/studentapp/MainActivity.java
@@ -10,6 +10,7 @@ import android.os.Bundle;
 import android.os.SystemClock;
 import android.support.v7.app.ActionBarActivity;
 import android.text.format.DateUtils;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -21,8 +22,11 @@ import com.aidangrabe.studentapp.wearable.DataLayerListenerService;
 
 public class MainActivity extends ActionBarActivity {
 
-    private static final String PREF_NEWS_ALARM_ACTIVE = "alarm.news";
-    private static final String PREF_TIMETABLE_ALARM_ACTIVE = "alarm.timetable";
+    private static final String PREF_NEWS_ALARM_TIME = "alarm_news";
+    private static final String PREF_TIMETABLE_ALARM_TIME = "alarm_timetable";
+
+    private static final long UPDATE_INTERVAL_TIMETABLE = DateUtils.MINUTE_IN_MILLIS;
+    private static final long UPDATE_INTERVAL_NEWS_DOWNLOADER = DateUtils.HOUR_IN_MILLIS * 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,31 +49,45 @@ public class MainActivity extends ActionBarActivity {
 
     }
 
+    // setup the news downloading and lecture notification alarms
     private void setupAlarms(Application context) {
 
+        long currentMilis = SystemClock.elapsedRealtime();
+
         SharedPreferences prefs = getPreferences(MODE_PRIVATE);
-        boolean newsAlarmOn = prefs.getBoolean(PREF_NEWS_ALARM_ACTIVE, false);
-        boolean timetableAlarmOn = prefs.getBoolean(PREF_TIMETABLE_ALARM_ACTIVE, false);
+        long newsAlarmMillis = prefs.getLong(PREF_NEWS_ALARM_TIME, currentMilis);
+        long timetableAlarmMillis = prefs.getLong(PREF_TIMETABLE_ALARM_TIME, currentMilis);
 
         AlarmManager alarmManager = (AlarmManager) context.getSystemService(ALARM_SERVICE);
 
-        if (!newsAlarmOn) {
-            setAlarm(alarmManager, NewsDownloaderService.class, DateUtils.HOUR_IN_MILLIS * 2);
-            prefs.edit().putBoolean(PREF_NEWS_ALARM_ACTIVE, true).apply();
+        if (alarmNeedsReset(newsAlarmMillis, UPDATE_INTERVAL_NEWS_DOWNLOADER)) {
+            setAlarm(alarmManager, NewsDownloaderService.class, UPDATE_INTERVAL_NEWS_DOWNLOADER);
+            prefs.edit().putLong(PREF_NEWS_ALARM_TIME, currentMilis).apply();
         }
 
-        if (!timetableAlarmOn) {
-            setAlarm(alarmManager, TimeTableService.class, DateUtils.MINUTE_IN_MILLIS * 5);
-            prefs.edit().putBoolean(PREF_TIMETABLE_ALARM_ACTIVE, true).apply();
+        if (alarmNeedsReset(timetableAlarmMillis, UPDATE_INTERVAL_TIMETABLE)) {
+            setAlarm(alarmManager, TimeTableService.class, UPDATE_INTERVAL_TIMETABLE);
+            prefs.edit().putLong(PREF_TIMETABLE_ALARM_TIME, currentMilis).apply();
         }
 
     }
 
+    /**
+     * Check if a given alarm needs to be reset
+     * @param lastStartMillis the last time (in millis) that the clock was set
+     * @param interval the time for which the alarm waits before triggering
+     * @return true if the alarm needs to be reset
+     */
+    private boolean alarmNeedsReset(long lastStartMillis, long interval) {
+        return SystemClock.elapsedRealtime() - lastStartMillis < interval;
+    }
+
     private void setAlarm(AlarmManager alarmManager, Class<? extends Service> serviceClass, long interval) {
+        Log.d("D", "Setting alarm");
         Intent intent = new Intent(this, serviceClass);
         PendingIntent pendingIntent = PendingIntent.getService(this, 0, intent, 0);
 
-        alarmManager.setInexactRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP, SystemClock.elapsedRealtime(), interval, pendingIntent);
+        alarmManager.setInexactRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP, SystemClock.elapsedRealtime() + interval, interval, pendingIntent);
     }
 
     @Override
